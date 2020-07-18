3221c4912eec7dba5354d1548a8264de7522142a
Christine <christineonaya5@gmail.com>
2020-07-18 16:02:54 +0300

    Add java script functions

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+months.forEach(function(value, index, array) {
+    console.log(value, index)
+})
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+days.forEach(function(value, index) {
+    console.log(value, index)
+    
+}) 
+var century digits = CC;
+var year digits = YY;
+var day of the month = DD;
+var month of the year = MM;
+var modulus function = mod;
